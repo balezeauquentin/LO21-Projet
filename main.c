1e669c96765541575ac1247cfe022bfe4d6342eb
Milenabravo <milena.bravo@utbm.fr>
2022-12-03 12:24:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,19 @@
 #include "liste_bit.h"
 
 int main() {
-    Initialisationpop(4);
+    //Initialisationpop(4);
+
+    //test liste de bits
+
+    Listebit l= initialisation_liste_bits(6);
+    afficher_list_bit(l);
+
+    printf("liste 2");
+    Listebit l2= initialisation_liste_bits(6);
+    afficher_list_bit(l2);
+
+    //test liste d'individus
+
+    //InitialisationIndiv()
     return 0;
 }
