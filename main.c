582500519908c9e72c323c3644c149e2dfe1ab72
balezeauquentin <94165700+balezeauquentin@users.noreply.github.com>
2022-12-31 14:24:17 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,23 +9,25 @@
 int main() {
     srand(time(NULL)); // initialisation du générateur aléatoire.
     Population p1;
-    p1=Initialisationpop(5);
-   // Afficher_population(p1);
+    int taillePop = (rand() % 180) + 20;
+    int tSelect = (rand() % 79) + 11;
+    int nGen = (rand() % 180) + 20;
+    printf("%d",taillePop);
+    p1=Initialisationpop(taillePop);
+
     Listebit list=NULL;
     list= initialisation_liste_bits_recursive(2);
     afficher_list_bit(list);
-    int seuil;
 
     int option=0;
       do{
-          printf("1) ""Initialiser la Population\n");
+          printf("1) Initialiser la Population\n");
           printf("2) Répéter nGen fois\n");
           printf("3) Début \n");
           printf("4) Croiser la Population \n");
           printf("5) Trier la Population \n");
           printf("6) Sélectionner la Population \n");
-          printf("7) Fin \n");
-          printf("8) Afficher le meilleur Individu de la Population \n");
+          printf("7) Afficher le meilleur Individu de la Population \n");
           printf("0) Quitter \n");
           scanf(" %d",&option);
           switch (option) {
@@ -56,8 +58,8 @@ int main() {
 
                   printf("Seuil :");
                   printf("-------------Selection de la population-------------");
-                  scanf("%d",&seuil);
-                  Selection(p1,seuil);
+                  scanf("%d",&tSelect);
+                  Selection(p1,tSelect);
                   break;
               case 8:
                   break;
